5ef0bb3343bbbb41e4f143b24f11ef8be9c3fa55
noandrea <no.andrea@gmail.com>
2025-01-20 16:27:43 +0100

    add display derive macro to enums

--- webhook-api/src/enums.rs
+++ webhook-api/src/enums.rs
@@ -2,14 +2,18 @@ use serde::{Deserialize, Serialize};
 use strum::{Display, EnumString};
 use utoipa::ToSchema;
 
-#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq, ToSchema, EnumString)]
+#[derive(
+    Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq, ToSchema, EnumString, Display,
+)]
 #[serde(rename_all = "camelCase")]
 pub enum QuoteType {
     ExactIn,
     ExactOut,
 }
 
-#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq, ToSchema, EnumString)]
+#[derive(
+    Debug, Copy, Clone, Serialize, Deserialize, PartialEq, Eq, ToSchema, EnumString, Display,
+)]
 #[serde(rename_all = "camelCase")]
 pub enum Protocol {
     V1,
