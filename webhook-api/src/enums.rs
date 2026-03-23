3e4c44b6aa3ede73844a51e113f0478680c538eb
ioanSL <ioan@raccoons.dev>
2026-03-23 11:53:46 +0200

    feat: add rejection for insufficient balance ata creation

--- webhook-api/src/enums.rs
+++ webhook-api/src/enums.rs
@@ -37,6 +37,7 @@ pub enum Protocol {
 #[serde(rename_all = "camelCase")]
 pub enum RejectionReason {
     InsufficientBalance,
+    InsufficientBalanceForAtaCreation,
     SignatureVerificationFailed,
     #[default]
     BotActivityDetected,
@@ -73,6 +74,14 @@ mod tests {
             insufficient_balance_json
         );
 
+        let insufficient_balance_ata =
+            SwapState::RejectedWithReason(RejectionReason::InsufficientBalanceForAtaCreation);
+        let insufficient_balance_ata_json = serde_json::to_string(&insufficient_balance_ata).unwrap();
+        println!(
+            "InsufficientBalanceForAtaCreation serialized: {}",
+            insufficient_balance_ata_json
+        );
+
         let signature_failed =
             SwapState::RejectedWithReason(RejectionReason::SignatureVerificationFailed);
         let signature_failed_json = serde_json::to_string(&signature_failed).unwrap();
@@ -81,14 +90,30 @@ mod tests {
             signature_failed_json
         );
 
+        let bot_activity =
+            SwapState::RejectedWithReason(RejectionReason::BotActivityDetected);
+        let bot_activity_json = serde_json::to_string(&bot_activity).unwrap();
+        println!(
+            "BotActivityDetected serialized: {}",
+            bot_activity_json
+        );
+
         // Test deserialization
         let deserialized_insufficient: SwapState =
             serde_json::from_str(&insufficient_balance_json).unwrap();
         assert_eq!(deserialized_insufficient, insufficient_balance);
 
+        let deserialized_insufficient_ata: SwapState =
+            serde_json::from_str(&insufficient_balance_ata_json).unwrap();
+        assert_eq!(deserialized_insufficient_ata, insufficient_balance_ata);
+
         let deserialized_signature: SwapState =
             serde_json::from_str(&signature_failed_json).unwrap();
         assert_eq!(deserialized_signature, signature_failed);
+
+        let deserialized_bot_activity: SwapState =
+            serde_json::from_str(&bot_activity_json).unwrap();
+        assert_eq!(deserialized_bot_activity, bot_activity);
     }
 
     #[test]
@@ -99,10 +124,22 @@ mod tests {
             "\"insufficientBalance\""
         );
 
+        let insufficient_balance_ata = RejectionReason::InsufficientBalanceForAtaCreation;
+        assert_eq!(
+            serde_json::to_string(&insufficient_balance_ata).unwrap(),
+            "\"insufficientBalanceForAtaCreation\""
+        );
+
         let signature_failed = RejectionReason::SignatureVerificationFailed;
         assert_eq!(
             serde_json::to_string(&signature_failed).unwrap(),
             "\"signatureVerificationFailed\""
+        );
+
+        let bot_activity = RejectionReason::BotActivityDetected;
+        assert_eq!(
+            serde_json::to_string(&bot_activity).unwrap(),
+            "\"botActivityDetected\""
         );
     }
 }
